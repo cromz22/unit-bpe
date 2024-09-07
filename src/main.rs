a6e866bf95a1a9b6e49fd39bd2f1bdb80c288f2a
cromz22 <tt.pfr4343@gmail.com>
2024-09-07 18:20:09 -0400

    merge

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::collections::HashMap;
 
-fn get_counts(units: Vec<i32>) -> HashMap<(i32, i32), i32> {
+fn get_counts(units: &[i32]) -> HashMap<(i32, i32), i32> {
     let mut counts: HashMap<(i32, i32), i32> = HashMap::new();
 
     for pair in units.windows(2) {
@@ -11,10 +11,30 @@ fn get_counts(units: Vec<i32>) -> HashMap<(i32, i32), i32> {
     counts
 }
 
+fn merge(units: &[i32], pair: (i32, i32), idx: i32) -> Vec<i32> {
+    let mut new_units = Vec::new();
+    let mut i = 0;
+    while i < units.len() {
+        if i < units.len() - 1 && (units[i], units[i + 1]) == pair {
+            new_units.push(idx);
+            i += 2;
+        } else {
+            new_units.push(units[i]);
+            i += 1;
+        }
+    }
+
+    new_units
+}
+
 fn main() {
     let units = vec![0, 1, 0, 1, 2, 0, 1, 2, 3, 0, 1, 2, 3, 4, 0, 1, 2, 3, 4, 5];
-    let counts = get_counts(units);
+    let counts = get_counts(&units);
     for ((k1, k2), value) in &counts {
         println!("{k1}, {k2}: {value}");
+    }
+    let new_units = merge(&units, (0, 1), 6);
+    for unit in new_units {
+        println!("{unit}");
     }
 }
